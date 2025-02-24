eb797bafbda1e716238ea993172d93cb61780a53
Jose Uriel Contreras Arevalos <128828694+Urielcont@users.noreply.github.com>
2025-02-24 14:41:18 -0600

    initial conf

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express";
+import cors from 'cors';
+
+// servidor de express
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true, 
+    methods: ["GET", "POST", "PUT",],
+  })
+);
+
+// Middleware del servidor
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando");
+});
+
+// Funcion para inicar el servidor en el puerto establecido
+const startServerExpress = async () => {
+  try {
+    app.listen(process.env.PORT, () => {
+      console.log("Servidor listo en el puerto: ",process.env.PORT);
+    });
+
+  } catch (error) {
+    console.error("Error al conectar el servidor", error);
+    process.exit(1); //Terminar el proceso
+  }
+};
+
+//Iniciar el servidor y configuraciones iniciales
+startServerExpress();
+
