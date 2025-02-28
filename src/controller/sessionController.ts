8ec592b18630abfe059f1870d3742a4f3908188f
Adrian Rocha <adrianrochacon@gmail.com>
2025-02-27 23:27:12 -0600

    endpoints para session

--- src/controller/sessionController.ts
+++ src/controller/sessionController.ts
@@ -1,1 +1,63 @@
-// Controller para Session de datos
+import { Request, Response } from "express";
+import logger from "../utils/logger";
+import { crearProfesor, loginProfesor } from "../services/sessionService";
+
+// Crear un nuevo profesor
+export const nuevoProfesor = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const profesorData = req.body;
+    const profesorNuevo = await crearProfesor(profesorData);
+
+    res.status(201).json({
+      message: "Profesor creado exitosamente",
+      profesor: profesorNuevo,
+    });
+  } catch (err) {
+    logger.error("Error al crear el usuario: ", err.message);
+    res.status(500).json({
+      error:
+        "Error interno al procesar la solicitud. Intenta nuevamente más tarde.",
+    });
+  }
+};
+
+export const ingresarProfesor = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({ error: "Email y contraseña son requeridos" });
+      return;
+    }
+
+    // Llamamos a la función para obtener el profesor
+    const profesor = await loginProfesor(email, password);
+
+    if (!profesor) {
+      res.status(401).json({ error: "Correo o contraseña incorrectos" });
+      return;
+    }
+
+    res.status(200).json({
+      message: "Inicio de sesión exitoso",
+      profesor,
+    });
+  } catch (err) {
+    logger.error("Error en el login: ", err.message);
+    res.status(500).json({ error: "Error en el servidor" });
+  }
+};
+
+export const logoutProfesorController = (req: Request, res: Response): void => {
+  try {
+    res.status(200).json({ message: "Sesión cerrada correctamente." });
+  } catch (error) {
+    res.status(500).json({ error: "Error al cerrar sesión." });
+  }
+};
---
+++ src/models/interface/sessionInterface.ts
@@ -0,0 +1,8 @@
+export interface Profesor {
+  id: number;
+  nombre: string;
+  apellido_paterno: string;
+  apellido_materno: string;
+  email: string;
+  password: string;
+}
